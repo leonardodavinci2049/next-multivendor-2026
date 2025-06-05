3fc303186b891fbd27cdd3d62388605396e8a351
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-06-05 09:57:38 -0300

    adiciona fonte Barlow ao tema e ao título da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 gap-8">
-      <h1 className="text-3xl text-green-900 font-bold">New Project Next</h1>
+      <h1 className="text-3xl text-green-900 font-bold font-barlow">New Project Next</h1>
       <div>
         <Button variant={"destructive"}>Click Here</Button>
       </div>
